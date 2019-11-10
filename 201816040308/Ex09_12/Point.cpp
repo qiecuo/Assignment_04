42907f4bc156bd121c24f2c41d581ca06f737a16
Shmilky-ops <56630024+Shmilky-ops@users.noreply.github.com>
2019-11-10 17:17:17 +0800

    Create Point.cpp

---
+++ 201816040308/Ex09_12/Point.cpp
@@ -0,0 +1,23 @@
+
+
+#include "Point.h"
+
+void Point::setx(double a)
+{
+    x=a;
+}
+
+void Point::sety(double a)
+{
+    y=a;
+}
+
+double Point::getx()
+{
+    return x;
+}
+
+double Point::gety()
+{
+    return y;
+}
