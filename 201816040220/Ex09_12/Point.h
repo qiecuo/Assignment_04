c3f87268338effad74fa95022990241b3f661713
今夕何夕 <53158211+LXGTLHL@users.noreply.github.com>
2019-11-10 22:26:19 +0800

    Create Point.h

---
+++ 201816040220/Ex09_12/Point.h
@@ -0,0 +1,12 @@
+#ifndef INTEGER_SET_H
+#define INTEGER_SET_H
+#include "Rectangle.h"
+#include "Point.h"
+class Point
+{
+public:
+    Point(float x1,float x2,float x3,float x4,float y1,float y2,float y3,float y4);
+
+};
+
+#endif
