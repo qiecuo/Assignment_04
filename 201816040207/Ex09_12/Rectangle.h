ec993e1078c1eee5fb0dd4331cc157a54aa70a67
Lee Hao <56342291+L1hao@users.noreply.github.com>
2019-11-11 22:28:27 +0800

    Add files via upload

---
+++ 201816040207/Ex09_12/Rectangle.h
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+
+
+class Rectangle
+{
+
+public:
+    Rectangle(int ,int );//构造函数
+    int perimeter();//周长
+    int area();//面积
+    void setlength(int );//长度赋值
+    void setwidth(int );//宽度赋值
+    int getlength();//得到长度
+    int getwidth();//得到宽度
+private:
+    int lengthdata;//长度
+    int widthdata;//宽度
+
+
+};
+
+
+
+
