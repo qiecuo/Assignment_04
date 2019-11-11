bdbce5086d89ddf6e5a8e3953b836ce037ca8a2c
201816040314 <56504172+201816040314@users.noreply.github.com>
2019-11-11 13:34:07 +0800

    Create DeckOfCards.h
    student id:201816040314

---
+++ 201816040314/Ex09_24/DeckOfCards.h
@@ -0,0 +1,24 @@
+#ifndef DECKOFCARD_H_INCLUDED
+#define DECKOFCARD_H_INCLUDED
+#include "Card.h"
+#include <vector>
+class DeckOfCards
+{
+
+public:
+    DeckOfCards();//default constructor
+    void shuffle();//shuffle function
+    Card &dealCard();//a function to return the next card
+    bool moreCards();//to judge whether there have cards
+    void hangOutCard();//a function to hang out the cards
+private:
+    //vector <Card> deck( 52 );
+    int currentCard;//data number currentCard
+    int currentCardSuit;//data number currentCardSuit
+    vector <Card> deck;// a vector to store the card
+    int number;//to record the number of the card
+
+};
+
+
+#endif // DECKOFCARD_H_INCLUDED
