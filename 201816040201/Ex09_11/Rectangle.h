f86ac88c4f24222aa67e5a5fc99a3c44906d8037
huatzy <56495508+huatzy@users.noreply.github.com>
2019-11-14 21:53:43 +0800

    Create Rectangle.h
    student ID：201816040201

---
+++ 201816040201/Ex09_11/Rectangle.h
@@ -0,0 +1,14 @@
+class Rectangle
+{
+public :
+    Rectangle(double =1.0,double =1.0);
+    void setlength(double);
+    void setwidth(double );
+    double getlength();
+    double getwidth();
+    double perimeter();
+    double area();
+private:
+    double length;
+    double width;
+};
