5a7ae9596b12f0af1cdda897710a32f94f3acab2
201816040314 <56504172+201816040314@users.noreply.github.com>
2019-11-16 21:18:20 +0800

    Update Card.cpp

--- 201816040314/Ex09_23/Card.cpp
+++ 201816040314/Ex09_23/Card.cpp
@@ -1,4 +1,7 @@
 #include "Card.h"
+
+string  Card::faceArray[13]={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+string  Card::suitArray[4]={"黑桃","红桃","梅花","方块"};
 Card::Card(int faceNumber,int suitNumber)//constructor with two parameter
 :face(faceNumber),suit(suitNumber)
 {
