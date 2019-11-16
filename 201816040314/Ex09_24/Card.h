48ac03d4111e046424839ef9f4d31480aec238f0
201816040314 <56504172+201816040314@users.noreply.github.com>
2019-11-16 21:35:58 +0800

    Update Card.h

--- 201816040314/Ex09_24/Card.h
+++ 201816040314/Ex09_24/Card.h
@@ -8,61 +8,28 @@ class Card
 {
 
 public:
-    string static faceArray[52];//a string static array to store the face of poker
-    string static suitArray[52];//a string static array to store the suit of poker
+    string static faceArray[13];//a string static array to store the face of the poker
+    string static suitArray[4];//a string static array to store the suit of the poker
     Card(int,int);//constructor with two parameter
-    string toString();//a function to return the value with a string form
-    int getFace();//to return the face of the poker
-    int getSuit();//to return the suit of the poker
+    string toString();//a function to return the string of the value
+    int getFace();//a function to return the face
+    int getSuit();//a function to return the suit
 private:
     int face;//data number face
     int suit;//data number suit
-    string toFace()//a tool function to return the face with a string form
+    string toFace()//a tool function to return the face with string form
     {
-        if(face > 10||face==1)
-        {
-            if(face==11)
-                return "J";
-            else if(face==12)
-                return "Q";
-            else if(face==1)
-                return "A";
-            else
-                return "K";
-        }
-        else
-        {
-
-            std::ostringstream ost;
-            ost << face;
-            string temp(ost.str());
-
-            return temp;
-        }
-
+        return faceArray[face-1];
     }
-    std::string toSuit() //a tool function to return the suit with a string form
+    std::string toSuit()//a tool function to return the suit with string form
     {
-        if(suit==1)
-        {
-            return "黑桃";
-        }
-        if(suit==2)
-        {
-            return "红桃";
-        }
-        if(suit==3)
-        {
-            return "梅花";
-        }
-        if(suit==4)
-        {
-            return "方块";
-        }
+        return suitArray[suit-1];
     }
 
 
 };
+
+#endif // CARD_H_INCLUDED
 
 
 #endif // CARD_H_INCLUDED
