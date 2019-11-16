05aea2cf99140e089ad12720d2013b95781056ad
huangqiongyan123 <56502228+huangqiongyan123@users.noreply.github.com>
2019-11-16 09:51:13 +0800

    Create Point.h
    This is my assignment_04
    ID:201816040325

---
+++ 201816040325/Ex09_12/Point.h
@@ -0,0 +1,18 @@
+#ifndef PIONT_H
+#define PIONT_H
+
+
+
+
+
+class Point
+{
+    public:
+        Point(double =0,double =0);
+        double getx();
+        double gety();
+    private:
+        double x;//表示x坐标值
+        double y;//表示y坐标轴
+};
+#endif // RECTANGLE_H_INCLUDED
