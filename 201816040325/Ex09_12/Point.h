3a7df1da2e0b3c6dd5fc7c125384bdd7d93c7e35
huangqiongyan123 <56502228+huangqiongyan123@users.noreply.github.com>
2019-11-16 09:44:30 +0800

    Create Point.h
    This is my assignment_04
    Student ID:201816040325

---
+++ 201816040325/Ex09_12/Point.h
@@ -0,0 +1,17 @@
+#include "Point.h"
+using namespace std;
+
+Point::Point(double m,double n)
+        :x(m),y(n)
+        {
+
+        }
+
+double Point::getx()
+{
+    return x;
+}
+double Point::gety()
+{
+    return y;
+}
