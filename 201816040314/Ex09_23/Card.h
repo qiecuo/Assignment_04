4dd25e0f115e5179b37fdf2891e408dea1477e60
201816040314 <56504172+201816040314@users.noreply.github.com>
2019-11-16 21:19:11 +0800

    Update Card.h

--- 201816040314/Ex09_23/Card.h
+++ 201816040314/Ex09_23/Card.h
@@ -8,8 +8,8 @@ class Card
 {
 
 public:
-    string static faceArray[52];//a string static array to store the face of the poker
-    string static suitArray[52];//a string static array to store the suit of the poker
+    string static faceArray[13];//a string static array to store the face of the poker
+    string static suitArray[4];//a string static array to store the suit of the poker
     Card(int,int);//constructor with two parameter
     string toString();//a function to return the string of the value
     int getFace();//a function to return the face
@@ -19,50 +19,13 @@ private:
     int suit;//data number suit
     string toFace()//a tool function to return the face with string form
     {
-        if(face > 10||face==1)
-        {
-            if(face==11)
-                return "J";
-            else if(face==12)
-                return "Q";
-            else if(face==1)
-                return "A";
-            else
-                return "K";
-        }
-        else
-        {
-
-            std::ostringstream ost;
-            ost << face;
-            string temp(ost.str());
-
-            return temp;
-        }
-
+        return faceArray[face-1];
     }
     std::string toSuit()//a tool function to return the suit with string form
     {
-        if(suit==1)
-        {
-            return "黑桃";
-        }
-        if(suit==2)
-        {
-            return "红桃";
-        }
-        if(suit==3)
-        {
-            return "梅花";
-        }
-        if(suit==4)
-        {
-            return "方块";
-        }
+        return suitArray[suit-1];
     }
 
-
 };
-
 
 #endif // CARD_H_INCLUDED
