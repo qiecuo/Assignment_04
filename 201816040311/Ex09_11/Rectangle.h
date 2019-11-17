9ce7895063f567dd3d41c2a79fa4ac28fb0fbb4a
liuccch <52843757+liuccch@users.noreply.github.com>
2019-11-17 17:32:00 +0800

    Update Rectangle.h

--- 201816040311/Ex09_11/Rectangle.h
+++ 201816040311/Ex09_11/Rectangle.h
@@ -1,1 +1,23 @@
+#ifndef RECTANGLE_H_INCLUDED
+#define RECTANGLE_H_INCLUDED
+
+class Rectangle
+{
+private:
+    double length;//Rectangle's length
+    double width;//Rectangle's width
+public:
+
+    Rectangle();//default constructor of the rectangle
+    Rectangle(double,double);//default constructor of the rectangle
+    void setLength(double);//set Rectangle's length
+    void setWidth(double);//set Rectangle's width
+    double getLength();//get Rectangle's length
+    double getWidth();//get Rectangle's width
+    double perimeter();//Calculate the perimeter
+    double area();//Calculate the area
+
+};
+
+#endif // RECTANGLE_H_INCLUDED
 
