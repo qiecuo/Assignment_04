a3e05e2e04b32daf9beecf3dc97e49cea3cf6c9c
cz-1999 <55949510+cz-1999@users.noreply.github.com>
2019-11-17 17:52:22 +0800

    Delete Ex09_11.cpp

--- 201816040209/Ex09_11/Ex09_11.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Rectangle.h"
-using namespace std;
-
-int main()
-{
-    Rectangle rectangle(4,5);//Create and initialize an object
-    cout<<"The perimeter is "<<rectangle.getPerimeter()<<"\n"<<"The area is "<<rectangle.getArea()<<endl;//return perimeter and area
-}
