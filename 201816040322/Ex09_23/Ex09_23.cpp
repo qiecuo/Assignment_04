8181beb93cce1d0c898734c8a1e7f5b2a76745f3
201816040322 <56496517+201816040322@users.noreply.github.com>
2019-11-17 17:29:26 +0800

    Create Ex09_23.cpp

---
+++ 201816040322/Ex09_23/Ex09_23.cpp
@@ -0,0 +1,14 @@
+// Assignment_04: Ex09_23.cpp
+// Create and manipulate DeckOfCards objects.
+#include <iostream>
+#include "DeckOfCards.h"
+
+using namespace std;
+
+int main()
+{
+    DeckOfCards deckofcard;
+    deckofcard.shuffle();
+    deckofcard.dealCard();
+    return 0;
+}
