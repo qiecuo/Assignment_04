99b9fd2c216e37c13a3cc344d4884413fe4efa84
liuccch <52843757+liuccch@users.noreply.github.com>
2019-11-17 17:37:38 +0800

    Update Ex09_23.cpp

--- 201816040311/Ex09_23/Ex09_23.cpp
+++ 201816040311/Ex09_23/Ex09_23.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include "Card.h"
+#include "DeckOfCards.h"
+
+using namespace std;
+
+int main()
+{
+    DeckOfCards b;
+    b.shuffle();
+    int c;
+    cout<<"Please input:"<<endl;
+    cin >> c;
+    for(int i=0;i<c;i++)
+    {
+        if(b.moreCards())
+        {
+            cout<<b.dealCard().toString()<<endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return 0;
+}
 
