9641c17587a66a1a7737978326ef11e377339e26
201816040230 <56499333+201816040230@users.noreply.github.com>
2019-11-17 18:03:27 +0800

    Create Ex09_23.cpp

---
+++ 201816040230/Ex09.23/Ex09_23.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "DeckOfCards.h"
+#include"Card.h"
+using namespace std;
+
+int main()
+{
+    DeckOfCards d;
+
+     for(int i=1;i<=53;i++)
+    {
+ d.print();
+    }
+    return 0;
+
+}
+//参考王嘉豪的
