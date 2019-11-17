44f57c194162e735f715ec301bab39526ec036d5
Ricardo-hs <56646943+Ricardo-hs@users.noreply.github.com>
2019-11-17 18:38:09 +0800

    Create Reatangle.h
    This is my assignment_04
    Student ID:201816040224

---
+++ 201816040224/Ex09_12/Reatangle.h
@@ -0,0 +1,21 @@
+//#ifndef RECTANGLE_H
+#define RECTANGLE_H
+//#ifndef RECTANGLE_H
+#include "Point.h"
+class Rectangle
+{
+public:
+    Rectangle(Point = Point(0.0,1.0),Point = Point(1.0,1.0),Point = Point(1.0,0.0),Point = Point(0.0,0.0));
+    void setCoord(Point,Point,Point,Point);
+    double length();
+    double width();
+    void perimeter();
+    void area();
+    bool square();
+private:
+    Point point1;
+    Point point2;
+    Point point3;
+    Point point4;
+};
+//#end if // RECTANGLE_H
