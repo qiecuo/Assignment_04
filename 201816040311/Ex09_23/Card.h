9e4207fe5910200a660a033a91e65a4f1b16fd04
liuccch <52843757+liuccch@users.noreply.github.com>
2019-11-17 17:37:11 +0800

    Update Card.h

--- 201816040311/Ex09_23/Card.h
+++ 201816040311/Ex09_23/Card.h
@@ -1,1 +1,24 @@
+#ifndef CARD_H_INCLUDED
+#define CARD_H_INCLUDED
+#include <array>
+
+using namespace std;
+
+static array<string,14>faceID={"0","1","2","3","4","5","6","7","8","9","10","J","Q","K"};
+static array<string,5>SUIT={"0","heart","spade","club","diamond"};
+
+class Card
+{
+private:
+    int face;
+    int suit;
+public:
+    Card();
+    Card(int, int);
+    void setface(int);
+    void setsuit(int);
+    string toString();
+};
+
+#endif // CARD_H_INCLUDED
 
