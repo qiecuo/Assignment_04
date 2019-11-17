20cbfb1280b17c5aff802c579b8b4eee0aae4184
201816040215 <56756113+201816040215@users.noreply.github.com>
2019-11-17 18:42:06 +0800

    Create Ex09_23.cpp
    This is my assignment_04
    Student ID:201816040215

---
+++ 201816040215/Ex09_23/Ex09_23.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+#include "Card.h"
+#include "DeckOfCard.h"
+int main()
+{
+    DeckOfCard palyer();
+    cout << player.shuffle() << player.dealCard << endl;
+}
