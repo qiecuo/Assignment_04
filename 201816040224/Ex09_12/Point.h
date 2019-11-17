583116890a497a54a5e133298e0c73fd3aa942f6
Ricardo-hs <56646943+Ricardo-hs@users.noreply.github.com>
2019-11-17 18:36:10 +0800

    Create Point.h
    This is my assignment_04
    Student ID:201816040224

---
+++ 201816040224/Ex09_12/Point.h
@@ -0,0 +1,17 @@
+#ifndef POINT_H_INCLUDED
+#define POINT_H_INCLUDED
+
+class Point
+{
+public:
+    point(double = 0.0,double = 0.0);
+    void setX(double);
+    void setY(double);
+    double getX();
+    double getY();
+private:
+    double x;
+    double y;
+};
+
+#endif // POINT_H_INCLUDED
