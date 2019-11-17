953cc171ab9c3f590cf3ed6fbf025695c3938fbd
liuccch <52843757+liuccch@users.noreply.github.com>
2019-11-17 18:03:30 +0800

    Update DeckOfCards.h

--- 201816040311/Ex09_23/DeckOfCards.h
+++ 201816040311/Ex09_23/DeckOfCards.h
@@ -8,13 +8,12 @@ class DeckOfCards
 {
 private:
     int currentCard=0;
-    vector<Card>deck;
+    vector<Card>deck;//create a vector
 public:
-    DeckOfCards();
-    void shuffle();
-    Card dealCard();
-    bool moreCards();
+    DeckOfCards();//default constructor of the DeckOfCards
+    void shuffle();//Random shuffle
+    Card dealCard();//Check if there is still work
+    bool moreCards();//retrun next card
 };
 
 #endif // DECKOFCARDS_H_INCLUDED
-
