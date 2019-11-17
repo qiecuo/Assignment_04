e25c590b8cf88298efbda2ff0d719409c6b5dc73
hhhhhepc <45264381+hhhhhpc@users.noreply.github.com>
2019-11-17 18:02:32 +0800

    Create Rectangle.cpp

---
+++ 201816040309/Ex09_12/Rectangle.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+#include "Rectangle.h"
+
+Rectangle::Rectangle(double l,double w)
+{
+    bool state = verifydate(l,w);
+    if(state == false)
+    {
+        cout<<"date is Invalid"<<endl;     //constructor
+    }
+    else
+    {
+        setwidth(w);
+        setlength(l);
+    }
+
+}
+void Rectangle::setwidth(double w)
+{
+    width = w;
+}
+void Rectangle::setlength(double l)
+{
+    length = l;
+}
+double Rectangle::getsquare()
+{
+    return width*length;     //get_square
+}
+double Rectangle::getcircumference()
+{
+    return width*2+length*2;    //get_circumference
+}
+
