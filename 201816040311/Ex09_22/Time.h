41265458270f1354d17c835993ffd6adfa6c663e
liuccch <52843757+liuccch@users.noreply.github.com>
2019-11-17 17:35:58 +0800

    Update Time.h

--- 201816040311/Ex09_22/Time.h
+++ 201816040311/Ex09_22/Time.h
@@ -1,1 +1,21 @@
+#ifndef TIME_H_INCLUDED
+#define TIME_H_INCLUDED
+
+class Time
+{
+private:
+    int temp;
+public:
+    Time();
+    Time(long int=0);
+    void setTime(long int);
+    int getHour();
+    int getMinute();
+    int getSecond();
+    void display();
+    void tick(int);
+
+};
+
+#endif // TIME_H_INCLUDED
 
