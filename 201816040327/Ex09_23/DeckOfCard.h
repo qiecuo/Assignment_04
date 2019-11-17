d33588deb6c930252c9339ca22f45b1e23a08308
MisonoMayuBW <44313792+MisonoMayuBW@users.noreply.github.com>
2019-11-17 19:14:19 +0800

    Create DeckOfCard.h

---
+++ 201816040327/Ex09_23/DeckOfCard.h
@@ -0,0 +1,31 @@
+//
+//  DeckOfCard.hpp
+//  Ex09_23
+//
+//  Created by misonomayubw on 2019/11/17.
+//  Copyright © 2019 misonomayubw. All rights reserved.
+//
+
+#ifndef DeckOfCard_hpp
+#define DeckOfCard_hpp
+
+#include <stdio.h>
+#include<iostream>
+#include"Card.hpp"
+
+using namespace std;
+
+class DeckOfCards
+{
+public:
+    //构造函数
+    DeckOfCards();
+    //洗牌
+    void shuffle();
+    
+    
+private:
+
+
+};
+#endif /* DeckOfCard_hpp */
