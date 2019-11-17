ec904af8ba2b5a682498639467e97031b1e76fed
MisonoMayuBW <44313792+MisonoMayuBW@users.noreply.github.com>
2019-11-17 16:06:35 +0800

    Create Point.h

---
+++ 201816040327/Point.h
@@ -0,0 +1,35 @@
+//
+//  main.h
+//  Ex09_12
+//
+//  Created by misonomayubw on 2019/11/17.
+//  Copyright © 2019 misonomayubw. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+#include<iostream>
+
+using namespace std;
+
+class Rectangle
+{
+public:
+    Rectangle (float, float);
+    float Perimeter;
+    float Area;
+    void setlength (float);
+    void setwidth (float);
+    void getlength (float);
+    void getwidth (float);
+    
+private:
+    float length;
+    float inlength;
+    float width;
+    float inwidth;
+}
+
+#endif /* main_h */
+
