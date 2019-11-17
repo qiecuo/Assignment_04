508b29d80a232bcc8f4a261c86db45c14d3ddecb
kore2048 <56683147+kore2048@users.noreply.github.com>
2019-11-17 18:12:04 +0800

    Create Point.h
    201816040304

---
+++ 201816040304/Ex09_12/Point.h
@@ -0,0 +1,18 @@
+#ifndef POINT_H
+#define POINT_H
+
+class Point
+{
+public:
+    void setPoint(double , double);//坐标SET函数
+    double getX();
+    double getY();
+
+private:
+    double X;
+    double Y;
+};
+
+
+
+#endif // POINT_H
