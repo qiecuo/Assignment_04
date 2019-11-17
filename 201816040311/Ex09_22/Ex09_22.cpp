747d2ecd9012a043cd4bb479d5d5e8638acda39a
liuccch <52843757+liuccch@users.noreply.github.com>
2019-11-17 17:35:10 +0800

    Update Ex09_22.cpp

--- 201816040311/Ex09_22/Ex09_22.cpp
+++ 201816040311/Ex09_22/Ex09_22.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <ctime>
+#include "Time.h"
+
+using namespace std;
+
+int main()
+{
+    time_t now = time(0);
+    Time a(now);
+    int b;
+    cout<<"--------------------------------\n"<<endl;
+    cin >> b;
+
+    cout << "UTC 日期和时间："<< now << endl;
+    a.tick(b);
+}
 
