5d13834bf8de7fe4201af99503cb37315cc78fca
liuccch <52843757+liuccch@users.noreply.github.com>
2019-11-17 17:38:25 +0800

    Update DeckOfCards.h

--- 201816040311/Ex09_23/DeckOfCards.h
+++ 201816040311/Ex09_23/DeckOfCards.h
@@ -1,1 +1,20 @@
+#ifndef DECKOFCARDS_H_INCLUDED
+#define DECKOFCARDS_H_INCLUDED
+#include <vector>
+#include "Card.h"
+using namespace std;
+
+class DeckOfCards
+{
+private:
+    int currentCard=0;
+    vector<Card>deck;
+public:
+    DeckOfCards();
+    void shuffle();
+    Card dealCard();
+    bool moreCards();
+};
+
+#endif // DECKOFCARDS_H_INCLUDED
 
