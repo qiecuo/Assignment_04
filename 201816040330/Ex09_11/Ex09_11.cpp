f1798b4122018c2edfbeee443f43328c7319c493
201816040330 <56644045+201816040330@users.noreply.github.com>
2019-11-17 19:03:42 +0800

    Create Ex09_11.cpp

---
+++ 201816040330/Ex09_11/Ex09_11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Rectangle.h"
+using namespace std;
+
+int main()
+{
+    Rectangle rectangle(4.5,9.0);
+    rectangle.getArea(4.5,9.0);
+    rectangle.getPerimeter(4.5,9.0);
+}
