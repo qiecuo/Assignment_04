7955c09998f4fd183a58dab8ffff3cc07fa4e26d
201816040229 <56762032+201816040229@users.noreply.github.com>
2019-11-17 18:19:55 +0800

    Create DeckOfCards.h

---
+++ 201816040229/Ex09_23/DeckOfCards.h
@@ -0,0 +1,14 @@
+#ifndef DECKOFCARDS_H_INCLUDED
+#define DECKOFCARDS_H_INCLUDED
+
+
+
+#endif // DECKOFCARDS_H_INCLUDED
+class DeckOfCards
+{
+public:
+    vector<Card> card;
+    Cards();
+    void displayall();
+
+};
