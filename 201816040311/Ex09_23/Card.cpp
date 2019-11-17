3dd351b74e982d3a60ebc1109921c8f4ab491273
liuccch <52843757+liuccch@users.noreply.github.com>
2019-11-17 17:36:49 +0800

    Update Card.cpp

--- 201816040311/Ex09_23/Card.cpp
+++ 201816040311/Ex09_23/Card.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include "Card.h"
+
+using namespace std;
+
+Card::Card()
+{
+
+}
+Card::Card(int a, int b)
+{
+    setface(a);
+    setsuit(b);
+}
+void Card::setface(int a)
+{
+    face=a;
+}
+void Card::setsuit(int b)
+{
+    suit=b;
+}
+
+string Card::toString()
+{
+    return faceID[face]+" of "+SUIT[suit];
+}
 
