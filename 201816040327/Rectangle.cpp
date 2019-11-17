0f94e2db49b4457dd796602630ddabea59eabd69
MisonoMayuBW <44313792+MisonoMayuBW@users.noreply.github.com>
2019-11-17 14:48:54 +0800

    Create Rectangle.cpp

---
+++ 201816040327/Rectangle.cpp
@@ -0,0 +1,61 @@
+//
+//  main.cpp
+//  Ex09_11
+//
+//  Created by misonomayubw on 2019/11/17.
+//  Copyright © 2019 misonomayubw. All rights reserved.
+//
+
+#include <iostream>
+#include "Rectangle.h"
+
+using namespace std;
+
+Rectangle::Rectangle (float length, float width);
+{
+    setlength (length);
+    setwidth (width);
+}
+
+float Rectangle::Perimeter()
+{
+    return 2*length+2*width;
+}
+
+float Rectangle::Area()
+{
+    return length*width;
+}
+
+void Rectangle::setlength(float inlength)
+{
+    // save input length data
+    if (inlength > 0 && inlength < 20)
+    {
+        length = inlength;
+    }
+    else
+        cout << "invalid input length" << endl;
+}
+float Rectangle::getlength()
+{
+    return length;
+    // load input length data
+}
+
+void Rectangle::setwidth(float inwidth)
+{
+    //save input width data
+    if (inwidth > 0 && inwidth < 20)
+    {
+        width = inwidth;
+    }
+    else
+        cout << "invalid input width" << endl;
+}
+
+float Rectangle::getwidth()
+{
+    return width;
+    //load input width data
+}
